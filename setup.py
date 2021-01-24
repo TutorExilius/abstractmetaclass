db3b7c5625b6fbc086e39f4ee5d1097150b808cc
Tutor Exilius <tutorexilius@gmail.com>
2021-01-24 18:05:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Tutor Exilius",
+    name="abstractmetaclass",
     version="0.0.1",
     author="Tutor Exilius",
     author_email="tutorexilius@gmail.com",
